26fb9668ba5843aa4b6e6f8123e668c439d21b00
federico <fedeeiguer@hotmail.com>
2022-09-22 18:10:49 -0300

    sopechozo

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import styles from "./about.module.css"
+import styles from "./about.module.css";
+import Link from "next/link";
 
 const about = () => {
   return (
     <>
       <div className={styles.container}>
         <div>about</div>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
       </div>
       <style jsx>{`
         div {
